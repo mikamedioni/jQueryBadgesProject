18eaf6f7c952a8b7456b4c8c02f866315b76b39d
Michael Mdn <michael.medioni@gmail.com>
2017-08-05 17:50:11 +0200

    Finishing the work

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,9 +7,23 @@ $(function() {
   	success: function(response){
   		var coursesCompletedArray = response.courses.completed;
   		coursesCompletedArray.forEach(function(course){
+  			
   			var courseDiv = $(document.createElement('div'));
-  			courseDiv.addClass('.course');
+  			courseDiv.addClass('course');
   			courseDiv.prepend('<h3>'+ course.title+'</h3>');
+  			
+  			var img = $(document.createElement('img'));
+  			img.attr({src : course.badge});
+  			img.prependTo(courseDiv);
+  			
+
+			var link = $(document.createElement('a'));
+  			link.attr({target : "_blank", href : course.url});
+  			link.addClass('btn btn-primary');
+  			link.text("See Course");
+
+  			link.appendTo(courseDiv);
+
   			courseDiv.appendTo($('#badges'));
   		});
 
