2b4af2f38a1aeff64fc0d2d0c39896a9d4a3017d
Michael Mdn <michael.medioni@gmail.com>
2017-08-05 17:06:48 +0200

    Add ajax call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+  	url: 'https://www.codeschool.com/users/jimika.json',
+  	dataType: 'jsonp',
+  	success: function(response){
+  		var coursesCompletedArray = response.courses.completed;
+  		coursesCompletedArray.forEach(function(course){
+  			var courseDiv = $(createElement('div'));
+  			courseDiv.addClass('.course');
+  			$('#badges').append(courseDiv);
+  		})
+
+  	}
+  });
 
 });
+
