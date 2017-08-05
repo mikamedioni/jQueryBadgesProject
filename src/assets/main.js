af7beb9b3b6505847bf862511b9d197d64f597ab
Michael Mdn <michael.medioni@gmail.com>
2017-08-05 17:23:08 +0200

    Add titles

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,10 +7,11 @@ $(function() {
   	success: function(response){
   		var coursesCompletedArray = response.courses.completed;
   		coursesCompletedArray.forEach(function(course){
-  			var courseDiv = $(createElement('div'));
+  			var courseDiv = $(document.createElement('div'));
   			courseDiv.addClass('.course');
-  			$('#badges').append(courseDiv);
-  		})
+  			courseDiv.prepend('<h3>'+ course.title+'</h3>');
+  			courseDiv.appendTo($('#badges'));
+  		});
 
   	}
   });
